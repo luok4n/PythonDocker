91f1b2d65b17dfb3f8fc65f0a43b62f0d032b83b
Sebastian Velez <sebasp1234@utp.edu.co>
2019-05-01 17:19:57 -0500

    helloworldpython

---
+++ dockerpython.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/hola-mundo')
+
+def hello_world():
+	nombre = request.args.get('nombre')
+	return "Hola " + nombre
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
