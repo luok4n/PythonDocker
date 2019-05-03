26eea08c8e22dedffe2e5a25e9c5c4c1f7d85fc9
Sebastian Velez Ramirez <sebasp1234@hotmail.es>
2019-05-03 14:35:25 -0500

    Update dockerpython.py

--- dockerpython.py
+++ dockerpython.py
@@ -2,10 +2,17 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+"""@app.route('/')
 
 def hello_world():
 	return "Hola mundo"
+"""
+
+@app.route('/hola-mundo')
+
+def hello_world():
+	nombre = request.args.get('nombre')
+	return "Hola " + nombre
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
